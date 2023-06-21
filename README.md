a802e5c4ef2822658dd3e8b37daa2398fad9092a
organictissuemaker <z3na.wu@gmail.com>
2023-06-21 11:22:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# VQE_EinR_2023
+ Code for EinR 2023 Internship
---
+++ textbook-main/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# Setup Python venv and install packages
+echo "Setting up Python virtual environment in ./.venv"
+python3.8 -m venv ./.venv
+. .venv/bin/activate
+python3.8 -m pip install -r environment/requirements.txt
+
+# Create default profile, then copy over our custom settings
+export IPYTHONDIR="./environment/ipython"
+echo "Creating new ipython profile at ${IPYTHONDIR}"
+ipython profile create
+cp ./environment/ipython/ipython_kernel_config.py $IPYTHONDIR/profile_default/ipython_kernel_config.py
+
+# Install git hooks
+pre-commit install
+
+echo ""
+echo "Qiskit Textbook setup complete!"
